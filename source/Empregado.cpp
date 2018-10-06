e44874e9a3959ba9369d41652ceb24de5f6ec329
Jonathan <perebaj@gmail.com>
2018-10-06 13:53:13 -0300

    Nao sei o que comentar mais eu gosto de commitar

--- source/Empregado.cpp
+++ source/Empregado.cpp
@@ -1,96 +1,136 @@
 #include <iostream>
 #include <string.h>
-#include <stdlib.h> //srand  rand
-#include <time.h> // time 
-//using namespace std;
-
-
-int salario_Alet(){
-    srand(time(NULL));
-    int cadastro = rand() % 1000;
-    return cadastro;
-}
-
+#include <stdlib.h> //SRAND RAND
+#include <time.h>   // TIME
 /*
- Verificar se o numero gerado para cadastro já existe
 
- @param cadastro - Cadatro que vai ser comparado
- @param new_cadastro -  Novo cadastro gerado
+    int salario_Alet()
+    {
+        srand(time(NULL));
+        int cadastro = rand() % 1000;
+        return cadastro;
+    }
 
-return 1 se os cadastros forem iguas
-return 0 se os cadastros forem diferentes
- */
-bool num_REP(int cadastro, int new_cadastro){
-    if  ( cadastro == new_cadastro){
+    /*
+    Verificar se o numero gerado para cadastro já existe
+
+    @param cadastro - Cadatro que vai ser comparado
+    @param new_cadastro -  Novo cadastro gerado
+
+    return 1 se os cadastros forem iguas
+    return 0 se os cadastros forem diferentes
+    
+bool num_REP(int cadastro, int new_cadastro)
+{
+    if (cadastro == new_cadastro)
+    {
         return true;
     }
-    else 
+    else
         return false;
 }
 
-class Empregado{
-    deix
+*/
+class Empregado
+{
+
     friend int cadastro_Alet();
-    public:
-        Empregado(std::string nome = "Anônimo", int tamanho  = 0);
-        Empregado(int tamanho, std::string a){
-            set_nome(a);
-            aloca_vetor(tamanho);
-        }
-        ~Empregado(){delete[] lista;}
-        void aloca_vetor(int _tam){
-            if(_tam > 0){
-                lista  = new std::string[_tam];
-                tamanho = _tam;
-                std::cout <<  "Vetor !!"<< std::endl;
-            }
-            std::cout << "Vetor nao alocado!" << std::endl;
-        }
-        void set_nome(std::string);
-        void set_lista();
-        void show_lista();
 
-        //Metodos get
+  public:
+    //Empregado(std::string nome = "Anônimo", int tamanho = 0);
+    Empregado(int tamanho, std::string a) {set_nome(a);set_aloca_vetor(tamanho);}
+    ~Empregado() { delete[] lista; }
 
-        int get_tamanho(){
-            return tamanho;
-        }
-        
-        
-    private:
-        std::string nome;
-        static int  cadastro ;
-        int tamanho; // tamanho da lista
-        double salario;
-        std::string *lista; // ponteira para primeira posição da lista de trabalhos do empregado
+    //MÉTODOS SET  
+    void set_aloca_vetor(int);
+    void set_nome(std::string);
+    void set_lista();
+    void show_lista() const;
+    void set_salario();
 
+    //MÉTODOD SET
+
+    int get_tamanho() const {return tamanho;}
+
+  private:
+    std::string nome;
+    static int cadastro;
+    int tamanho; // TAMANHO DA LISTA
+    double salario;
+    std::string *lista; // PONTEIRO PARA PRIMEIRA POSIÇÃO DA LISTA
 };
 
-    int Empregado::cadastro = 0;
-    /*
-     Le Nome
+int Empregado::cadastro = 0;
+/*
+    OBJ     LER O NOME DO EMPREGADO
+    @PARAM      _NAME       NOME DO EMPREGADO
+*/
+void Empregado::set_nome(std::string _name)
+{
+    std::getline(std::cin, _name);
+    nome = _name;
+}
+/*
+     OBJ     ALOCAR ESPAÇO DE MEMORIA PARA O VETOR CASO O SEU TAMANHO SEJA VÁLIDO
+    @PARAM      _tam        TAMANHO DO VETOR QUE VAI SER VERIFICADO
     */
-    void Empregado::set_nome(std::string _name){
-        std::getline(std::cin, _name);
-        nome = _name;
-    }
-    
-    void Empregado::set_lista(){
-        std::cout<<"Digite o nome da(s) empresas que trabalhou" <<endl;
-        for(int i = 0; i < get_tamanho(); i++){
-            std::cout<< "#" << i+1 << "Empresa" << endl;
-            std::getline(std::cin, lista[i]);
+void Empregado::set_aloca_vetor(int _tam)
+    {
+        if (_tam > 0)
+        {
+            lista = new std::string[_tam];
+            tamanho = _tam;
+            std::cout << "Vetor !!" << std::endl;
         }
+        else
+            std::cout << "Vetor nao alocado!" << std::endl;
     }
-    void Empregado::show_lista(){
-        for(int i = 0; i < get_tamanho(); i++)
-            std::cout<< lista[i] << std::endl;
+/*
+    OBJ:        LER O NOME DAS EMPRESAS QUE O FUNCIONÁRIO JA TRABALHOU
+
+    */
+void Empregado::set_lista()
+{
+    std::cout << "Digite o nome da(s) empresas que trabalhou" << std::endl;
+    for (int i = 0; i < get_tamanho(); i++)
+    {
+        std::cout << "#" << i + 1 << " Empresa" << std::endl;
+        std::getline(std::cin, lista[i]);
     }
-
-int main(){
-
-    Empregado();
+}
+/*
+    MOSTRAR A LISTA DE EMPRESAS
+    */
+void Empregado::show_lista() const
+{
+    for (int i = 0; i < get_tamanho(); i++)
+        std::cout << lista[i] << std::endl;
+}
+/*
+OBJ     VÁLIDAR SALÁRIO 
+*/
+void Empregado::set_salario(){
+    double salary;
+    std::cin >> salary;
+    if(salary > 0)
+        this->salario = salary;
 }
 
-
-
+int main()
+{
+    
+    Empregado e1(5,"JONATHAN"); // CRIAÇÃO DO OBJETO
+    
+    int aux = 5;
+    while(aux != 0){
+        std::cout<<"1)Nome das empresas que o funcionário ja trabalhou"<< std::endl;
+        std::cout<<"2)Salario"<< std::endl;
+        std::cout<<"0)Sair"<< std::endl;
+        std::cin >>aux;
+        if(aux == 1)
+            e1.set_lista();
+        else if(aux == 2)
+            e1.set_salario();          
+    }
+    
+}
