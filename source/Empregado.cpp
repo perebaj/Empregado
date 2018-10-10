92f0698b7d853e4b8897b14115698690d2fb8529
Jonathan <perebaj@gmail.com>
2018-10-09 22:53:54 -0300

    falta só uma funçaum

--- source/Empregado.cpp
+++ source/Empregado.cpp
@@ -14,6 +14,7 @@ class Empregado
     {
         set_nome(a);
         set_aloca_vetor(tamanho);
+        cadastro++;
     }
     ~Empregado() { delete[] lista; }
 
@@ -21,29 +22,30 @@ class Empregado
     void set_aloca_vetor(int);
     void set_nome(std::string);
     void set_lista();
-    void show_lista() const;
+    void show() const;
     void set_salario();
 
-    //MÉTODOD SET
-
-    int get_tamanho() const { return tamanho; }
-
+    //MÉTODOD GET
+    int get_cadastro() const{return cadastro;}
+    int get_tamanho() const {return tamanho;}
+    std::string get_nome() const {return nome;}
+    double get_salario() const {return salario;}
   private:
     std::string nome;
-    static int cadastro;
+    static int cadastro;//NUMERO SEQUENCIAL DE CADASTRO DE CADA FUNCIONÁRIO
     int tamanho; // TAMANHO DA LISTA
     double salario;
     std::string *lista; // PONTEIRO PARA PRIMEIRA POSIÇÃO DA LISTA
 };
 
 int Empregado::cadastro = 0;
+
 /*
     OBJ     LER O NOME DO EMPREGADO
     @PARAM      _NAME       NOME DO EMPREGADO
 */
 void Empregado::set_nome(std::string _name)
 {
-    std::getline(std::cin, _name);
     nome = _name;
 }
 /*
@@ -56,29 +58,36 @@ void Empregado::set_aloca_vetor(int _tam)
     {
         lista = new std::string[_tam];
         tamanho = _tam;
-        std::cout << "Vetor !!" << std::endl;
     }
     else
         std::cout << "Vetor nao alocado!" << std::endl;
 }
+
 /*
     OBJ:        LER O NOME DAS EMPRESAS QUE O FUNCIONÁRIO JA TRABALHOU
 
     */
+/*
+    OBJ         SETAR OS ELEMENTOS DA LISTA
+    */
 void Empregado::set_lista()
 {
-    std::cout << "Digite o nome da(s) empresas que trabalhou" << std::endl;
+    std::cout << "Digite o nome da(s) empresas que trabalhou: " << std::endl;
     for (int i = 0; i < get_tamanho(); i++)
     {
         std::cout << "#" << i + 1 << " Empresa" << std::endl;
-        std::getline(std::cin, lista[i]);
+        std::cin >> lista[i];
+        //std::getline(std::cin, lista[i]);
     }
 }
 /*
-    MOSTRAR A LISTA DE EMPRESAS
+    MOSTRAR A LISTA DE EMPRESAS QUE O FUNCINÁRIO TRABALHOU
     */
-void Empregado::show_lista() const
+void Empregado::show() const
 {
+    std::cout << "Empregado: " << get_cadastro() << std::endl;
+    std::cout << "Nome: " << get_nome() << std::endl;
+    std::cout << "Salario: " << get_salario() << std::endl;
     for (int i = 0; i < get_tamanho(); i++)
         std::cout << lista[i] << std::endl;
 }
@@ -88,6 +97,7 @@ OBJ     VÁLIDAR SALÁRIO
 void Empregado::set_salario()
 {
     double salary;
+    std::cout<< "Digite o salario: " << std::endl;
     std::cin >> salary;
     if (salary > 0)
         this->salario = salary;
@@ -101,13 +111,17 @@ int main()
     int aux = 5;
     while (aux != 0)
     {
-        std::cout << "1)Nome das empresas que o funcionário ja trabalhou" << std::endl;
+        std::cout << "1)Inserir o nome das empresas que o funcionario ja trabalhou" << std::endl;
         std::cout << "2)Salario" << std::endl;
+        std::cout << "3)Informaçoes do funcinario" << std::endl;
         std::cout << "0)Sair" << std::endl;
         std::cin >> aux;
         if (aux == 1)
             e1.set_lista();
-        else if (aux == 2)
+        if (aux == 2)
             e1.set_salario();
+        if (aux == 3)
+            e1.show();
     }
+    
 }
